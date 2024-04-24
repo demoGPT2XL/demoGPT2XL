3ca55ab08ff8d5a67696ad97c471d498123116fe
demoGPT2XL <167976688+demoGPT2XL@users.noreply.github.com>
2024-04-24 16:43:40 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_URL = "https://api-inference.huggingface.co/models/migueldeguzmandev/GPT2XL_RLLMv18-3"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        output = query({"inputs": input_text})
+        return render_template('index.html', output=output)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
